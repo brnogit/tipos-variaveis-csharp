b47abc252a4dafb113a695ad646dbad675757693
bmotadev <bmotadev@gmail.com>
2022-08-15 12:40:38 -0300

    Add project files.

---
+++ TiposEVariaveisCsharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposEVariaveisCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroInt = 10;
+
+            int maiorNumeroInt = int.MaxValue;
+            int menorNumeroInt = int.MinValue;
+
+            long numeroLong = 123123123123123;
+
+            long maiorNumeroLong = long.MaxValue;
+            long menorNumeroLong = long.MinValue;
+
+            decimal numeroDecimal = 10.52m;
+
+            double numeroDouble = 12.3;
+            double menorNumeroDouble = double.MinValue;
+
+            bool verdadeiro = true;
+            bool falso = false;
+
+            var numero = 10;
+
+            string nome = "Bruno Mota";
+            char letra = 'B';
+
+            DateTime entradaEmpresa = new DateTime(2021, 1, 1);
+            TimeSpan quantoTempoDeEmpresa = DateTime.Now - entradaEmpresa;
+
+
+            Console.Read();
+        }
+    }
+}
